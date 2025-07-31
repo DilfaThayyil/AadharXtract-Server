3d567c1baaa09426ffaabaacda37af850eeec2ab
DilfaThayyil <dilfathayyil@gmail.com>
2025-07-31 13:00:35 +0530

    feat: Aadhar validations added after extracting via cloud vision api

--- src/application/use-cases/ExtractAadharData.ts
+++ src/application/use-cases/ExtractAadharData.ts
@@ -1,6 +1,7 @@
 import { IOcrService } from '../../infrastructure/interfaces/IOcrService';
 import { extractAadhaarDetails } from '../../utils/extractAadhar';
 import { OcrService } from '../../infrastructure/services/ocr.service';
+import { isValidAadhaarBack, isValidAadhaarFront } from '../../utils/validateAadharText';
 
 export class ExtractAadharData {
   static async execute(frontBuffer: Buffer, backBuffer: Buffer) {
@@ -8,7 +9,15 @@ export class ExtractAadharData {
 
     const frontText = await ocrService.extractTextFromImage(frontBuffer);
     const backText = await ocrService.extractTextFromImage(backBuffer);
-
+    if (!isValidAadhaarFront(frontText) && !isValidAadhaarBack(backText)) {
+      throw new Error("Both sides does not appear to be a valid Aadhaar card.");
+    }
+    if (!isValidAadhaarBack(backText)) {
+      throw new Error("Back side does not appear to be a valid Aadhaar card.");
+    }
+    if (!isValidAadhaarFront(frontText)) {
+      throw new Error("front side does not appear to be a valid Aadhaar card.");
+    }
     const data = extractAadhaarDetails(frontText, backText);
     const frontBase64 = `data:image/png;base64,${frontBuffer.toString('base64')}`;
     const backBase64 = `data:image/png;base64,${backBuffer.toString('base64')}`;
